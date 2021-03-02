6f46f46bb38db723f8d130934cfa61d8be7870d7
lucacabibbo <luca.cabibbo@uniroma3.it>
2021-03-02 08:50:46 +0100

    Asw 2021 commit iniziale
    Architettura dei sistemi software (2021)

---
+++ shared/scripts/setup-open-jdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source "/home/asw/shared/scripts/common.sh"
+
+# see https://openjdk.java.net/install/ 
+
+# set up Java constants 
+# OPENJDK_PACKAGE=openjdk-8-jdk
+OPENJDK_PACKAGE=openjdk-14-jdk
+
+echo "==================="
+echo "installing open jdk"
+echo "==================="
+
+apt update 
+apt install -y ${OPENJDK_PACKAGE}
