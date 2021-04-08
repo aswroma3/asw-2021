013ca7a2173dfed64f881bda5de5d5890f573d6b
lucacabibbo <luca.cabibbo@uniroma3.it>
2021-04-08 13:19:40 +0200

    Aggiornato progetto asw-830
    Piccole modifiche nei client

--- projects/asw-830-grpc/b-restaurant/restaurant-client-grpc/src/main/java/asw/efood/restaurantservice/client/domain/RestaurantClientRunner.java
+++ projects/asw-830-grpc/b-restaurant/restaurant-client-grpc/src/main/java/asw/efood/restaurantservice/client/domain/RestaurantClientRunner.java
@@ -16,12 +16,16 @@ public class RestaurantClientRunner implements CommandLineRunner {
 	private RestaurantServiceAdapter restaurantServiceAdapter;
 
 	public void run(String[] args) throws InterruptedException {
+		logger.info("*** Cerco il ristorante con id=1L ***");
 		logger.info(restaurantServiceAdapter.getRestaurant(1L).toString());
 
+		logger.info("*** Creo un nuovo ristorante Alfa ***");
 		Long newRestaurantId = restaurantServiceAdapter.createRestaurant("Alfa", "Roma");
 		logger.info(newRestaurantId.toString());
+		logger.info("*** Cerco il ristorante appena creato ***");
 		logger.info(restaurantServiceAdapter.getRestaurant(newRestaurantId).toString());
 
+		logger.info("*** Cerco tutti i ristoranti ***");
 		logger.info(restaurantServiceAdapter.getAllRestaurants().toString());
 	}
 }
--- projects/asw-830-grpc/c-restaurant-with-menu/restaurant-client-grpc/src/main/java/asw/efood/restaurantservice/client/domain/RestaurantClientRunner.java
+++ projects/asw-830-grpc/c-restaurant-with-menu/restaurant-client-grpc/src/main/java/asw/efood/restaurantservice/client/domain/RestaurantClientRunner.java
@@ -16,19 +16,26 @@ public class RestaurantClientRunner implements CommandLineRunner {
 	private RestaurantServiceAdapter restaurantServiceAdapter;
 
 	public void run(String[] args) throws InterruptedException {
+		logger.info("*** Cerco il ristorante con id=1L ***");
 		logger.info(restaurantServiceAdapter.getRestaurant(1L).toString());
+		logger.info("*** Cerco il menu del ristorante con id=2L ***");
 		logger.info(restaurantServiceAdapter.getRestaurantMenu(2L).toString());
 
+		logger.info("*** Creo un nuovo ristorante Alfa ***");
 		Long newRestaurantId = restaurantServiceAdapter.createRestaurant("Alfa", "Roma");
 		logger.info(newRestaurantId.toString());
+		logger.info("*** Cerco il ristorante appena creato ***");
 		logger.info(restaurantServiceAdapter.getRestaurant(newRestaurantId).toString());
 
+		logger.info("*** Creo il menu del ristorante appena creato ***");
 		List<MenuItem> menuItems = new ArrayList<>();
 		menuItems.add( new MenuItem("SPV", "Spaghetti alle vongole", 11.0) );
 		menuItems.add( new MenuItem("LIS", "Linguine allo scoglio", 12.0) );
 		logger.info(restaurantServiceAdapter.createRestaurantMenu(newRestaurantId, menuItems).toString());
+		logger.info("*** Cerco il menu del ristorante appena creato ***");
 		logger.info(restaurantServiceAdapter.getRestaurantMenu(newRestaurantId).toString());
 
+		logger.info("*** Cerco tutti i ristoranti ***");
 		logger.info(restaurantServiceAdapter.getAllRestaurants().toString());
 	}
 }
