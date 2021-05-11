7ee4d21cacb34edb325f67215f04da1c29f8fc01
lucacabibbo <luca.cabibbo@uniroma3.it>
2021-05-11 09:26:11 +0200

    Aggiornato progetto asw-870
    Corretto apt con apt-get

--- projects/asw-870-strumenti-ambienti-virtuali/a-apache-shell/install-apache.sh
+++ projects/asw-870-strumenti-ambienti-virtuali/a-apache-shell/install-apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt install -y apache2
+apt-get update
+apt-get install -y apache2
 if ! [ -L /var/www/html ]; then
   rm -rf /var/www/html
   ln -fs /vagrant/www /var/www/html
